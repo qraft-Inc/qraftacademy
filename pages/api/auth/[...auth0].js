1c74b4eec77d5f275b11d177b7dba5b16b135203
mambop <pbiscolt@gmail.com>
2021-12-31 19:35:30 +0300

    Fix:invalid image extension

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -1,9 +1,10 @@
-import { handleAuth,handleLogin } from '@auth0/nextjs-auth0';
+import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
 
 export default handleAuth({
-    async login(req, res) {
-      await handleLogin(req, res, {
-        returnTo: "https://www.dashboard.qraftacademy.com/dashboard",
-      });
-    },
-  });
+  async login(req, res) {
+    await handleLogin(req, res, {
+      // returnTo: "https://www.dashboard.qraftacademy.com/dashboard",
+      returnTo: "https://www.qraftacademy.com",
+    });
+  },
+});
