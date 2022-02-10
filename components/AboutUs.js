52c049f15325dff7483cf8b76d53aa8b9e84619a
mambop <pbiscolt@gmail.com>
2022-02-10 23:32:18 +0300

    Chore:profile updates

--- components/AboutUs.js
+++ components/AboutUs.js
@@ -6,92 +6,75 @@ export default function AboutUs() {
         //start of  about area 
         <div className="flex justify-items-center text-gray-700 justify-center items-center font-semiMedium text-lg font-sans text-left tracking-normal leading-relaxed mb-6 lg:mb-0">
             <div className="grid max-w-xl lg:min-w-full lg:grid-cols-2 gap-x-8 justify-between min-h-full px-10">
-                <div className="">
-                    <div>
-                        <h3 className="text-4xl font-bold mb-6 lg:mt-8 text-light-blue">
-                            What
-                            <span className="text-blue-900"> QraftAcademy </span>
-                            <span className="">Is </span>
-                            For You
-                        </h3>
-                        <div className=" space-y-4 mb-3">
-                            <blockquote>Qraft Academy is an education startup that provides participants with an opportunity for self-discovery, access to experiential learning opportunities, and meaningful work.
-                                <br />
-                                The objective is to expose participants to a diverse range of opportunities to build experience, clarity about their career goals, and achieve them.
-                            </blockquote>
-                            <blockquote>
-                                This is done through hands-on training, mentorship, and apprenticeship programs. Upon graduation some are maintained, others connected to paid work opportunities.
-                                <br />
-                                Qraft Academy is an initiative from Qraft Mind, a modern work award winner.
-                            </blockquote>
+                <div>
+                    <h3 className="text-4xl font-bold mb-6 lg:mt-8 text-light-blue">
+                        What
+                        <span className="text-blue-900"> QraftAcademy </span>
+                        <span className="">Is </span>
+                        For You
+                    </h3>
+                    <div className=" space-y-4 mb-3">
+                        <blockquote>Qraft Academy is an education startup that provides participants with an opportunity for self-discovery, access to experiential learning opportunities, and meaningful work.
+                            <br />
+                            The objective is to expose participants to a diverse range of opportunities to build experience, clarity about their career goals, and achieve them.
+                        </blockquote>
+                        <blockquote>
+                            This is done through hands-on training, mentorship, and apprenticeship programs. Upon graduation some are maintained, others connected to paid work opportunities.
+                            <br />
+                            Qraft Academy is an initiative from Qraft Mind, a modern work award winner.
+                        </blockquote>
+                    </div>
+                    <div className="accordion" id="accordionExample">
+                        <div className="accordion-item">
+                            <h2 className="accordion-header mb-0" id="headingOne">
+                                <button className="accordion-button collapsed relative flex items-center w-auto py-4 px-5 text-base text-left transition focus:outline-none bg-blue-900 text-white "
+                                    type="button"
+                                    data-bs-toggle="collapse"
+                                    data-bs-target="#collapseOne"
+                                    aria-expanded="true"
+                                    aria-controls="collapseOne">Learn More..</button>
+                            </h2>
+                            <div
+                                id="collapseOne"
+                                className="accordion-collapse collapse show"
+                                aria-labelledby="headingOne"
+                                data-bs-parent="#accordionExample"
+                            >
+                                <div className="accordion-body py-4 px-5">
+                                    <h3 className="text-xl font-bold mb-6 lg:mt-8 text-light-blue">
+                                        Through our online qraft academy platform young people access;
+                                    </h3>
+                                    <ul className="list-disc space-y-2">
+                                        <li>Online remote apprenticeships to gain work experience and work based learning with tracked competencies. This comes with deliberate talent development and matching or access to  opportunities like paid employment, freelance and scholarships.</li>
+                                        <li>Hybrid business design sprints for venture development to start or accelerate self employment/ entrepreneurship as a career </li>
+                                        <li>E-commerce to sell their products</li>
+                                        <li>MOOCs for personal , professional and business development</li>
+                                        <li>One on one career mentorship and guidance</li>
+                                    </ul>
+                                </div>
+                            </div>
                         </div>
-                        <Link href="/">
-                            <a className="text-white bg-blue-900 inline-flex items-center justify-center  px-8 py-4 text-base font-semibold leading-6 rounded-full w-auto mt-2 mb-2  hover:bg-blue-500 transition duration-300">Learn More..</a>
-                        </Link>
-                        <h3 className="text-xl font-bold mb-6 lg:mt-8 text-light-blue">
-                            Through our online qraft academy platform young people access;
-                        </h3>
-                        <ul className="list-disc space-y-2">
-                            <li>Online remote apprenticeships to gain work experience and work based learning with tracked competencies. This comes with deliberate talent development and matching or access to  opportunities like paid employment, freelance and scholarships.</li>
-                            <li>Hybrid business design sprints for venture development to start or accelerate self employment/ entrepreneurship as a career </li>
-                            <li>E-commerce to sell their products</li>
-                            <li>MOOCs for personal , professional and business development</li>
-                            <li>One on one career mentorship and guidance</li>
-                        </ul>
-
-                
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
                     </div>
                 </div>
 
                 {/*start of about image area*/}
-                <div className="flex justify-center">
-                    <div className="flex justify-center ">
-                        <Image
-                            src="https://res.cloudinary.com/filipe256/image/upload/v1642172211/qraftstore/team_qraftacademy_boqbc4.jpg"
-                            alt="Team Qraft Academy - About us image background"
-                            width={550}
-                            height={450}
-                            quality={100}
-                            objectFit="cover"
-                            className="object-cover mt-4 md:object-none"
-                        />
-                    </div>
+
+                <div>
+                    <Image
+                        src="https://res.cloudinary.com/filipe256/image/upload/v1642172211/qraftstore/team_qraftacademy_boqbc4.jpg"
+                        alt="Team Qraft Academy - About us image background"
+                        width={550}
+                        height={450}
+                        quality={100}
+                        objectFit="cover"
+                        className="object-cover mt-4 md:object-none"
+                    />
                 </div>
                 {/* about image area end */}
 
             </div>
         </div>
-        // end of about area 
+        // end of about area
     );
 }
--- components/OurTeam.js
+++ components/OurTeam.js
@@ -1,9 +1,9 @@
 import Image from "next/image";
 import Link from "next/link";
-import {AiFillTwitterSquare} from "react-icons/ai";
-import {AiFillFacebook} from "react-icons/ai";
-import {AiFillInstagram} from "react-icons/ai";
-import {AiFillLinkedin} from "react-icons/ai";
+import { AiFillTwitterSquare } from "react-icons/ai";
+import { AiFillFacebook } from "react-icons/ai";
+import { AiFillInstagram } from "react-icons/ai";
+import { AiFillLinkedin } from "react-icons/ai";
 
 // data to be displayed on each mentor card
 const data = [
@@ -37,7 +37,7 @@ const data = [
     },
     {
         id: 4,
-        courseImg: "https://res.cloudinary.com/filipe256/image/upload/v1644153416/qraftstore/Ronald_Bataringaya_bgwwj7.jpg",
+        courseImg: "https://res.cloudinary.com/filipe256/image/upload/v1644524458/qraftstore/Ronald_kr0pu9.jpg",
         name: "Ronald Bataringaya ",
         role: "Management & Operations",
         altText: "Ronald - Management & Operations"
@@ -72,132 +72,132 @@ const data = [
     },
     {
         id: 9,
+        courseImg: "https://res.cloudinary.com/filipe256/image/upload/v1642172209/qraftstore/Juliyana_woldeab_pwoiyd.jpg",
+        name: "Juliyana Woldeab",
+        role: "Digital Marketing",
+        altText: "Juliyana - Digital Marketing"
+    },
+    {
+        id: 10,
+        courseImg: "https://res.cloudinary.com/filipe256/image/upload/v1644427177/qraftstore/peter_dz3vro.jpg",
+        name: "Peter Ojwang",
+        role: "Digital Marketing",
+        altText: "Peter - Digital Marketing"
+    },
+    {
+        id: 11,
         courseImg: "https://res.cloudinary.com/filipe256/image/upload/v1644157368/qraftstore/igxjeiazjt5oiv8ejwwn.png",
         name: "Joshua Makubuya ",
         role: "Graphics Designer",
         altText: "Joshua - Graphics Designer"
     },
     {
-        id: 10,
-        courseImg: "https://res.cloudinary.com/filipe256/image/upload/v1642172209/qraftstore/Juliyana_woldeab_pwoiyd.jpg",
-        name: "Juliyana Woldeab",
-        role: "Digital Marketer",
-        altText: "Juliyana - Digital Marketing"
+        id: 12,
+        courseImg: "https://res.cloudinary.com/filipe256/image/upload/v1644157368/qraftstore/igxjeiazjt5oiv8ejwwn.png",
+        name: "Ceaser Mwaka",
+        role: "Graphic Designer",
+        altText: "Ceaser - Graphic Designer"
     },
     {
-        id: 11,
+        id: 13,
         courseImg: "https://res.cloudinary.com/filipe256/image/upload/v1642172210/qraftstore/Samuel_Mabonga_UX-UI_Designer_uiwc2q.jpg",
         name: "Samuel Mabonga",
         role: "UX/UI Designer",
         altText: "Samuel - UX/UI"
     },
     {
-        id: 12,
+        id: 14,
         courseImg: "https://res.cloudinary.com/filipe256/image/upload/v1642172209/qraftstore/Precious_Kasasira_Developer_mqxjdy.jpg",
         name: "Precious Kasasira",
         role: "Software Developer",
         altText: "Precious - Developer"
     },
     {
-        id: 13,
+        id: 15,
         courseImg: "https://res.cloudinary.com/filipe256/image/upload/v1642172208/qraftstore/phillip_Mambo_Developer_wpw8ez.jpg",
         name: "Phillip Mambo",
         role: "Software Developer",
         altText: "Phillip - Developer"
     },
     {
-        id: 14,
-        courseImg: "https://res.cloudinary.com/filipe256/image/upload/v1642172204/qraftstore/Anold_Kalema_Developer_bwknkb.jpg",
-        name: "Anold Kalema",
-        role: "Software Developer",
-        altText: "Anold - Developer"
-    },
-    {
-        id: 15,
-        courseImg: "https://res.cloudinary.com/filipe256/image/upload/v1642172206/qraftstore/Joshua_Kasasira_Developer_ehgije.jpg",
-        name: "Joshua Kasasira",
-        role: "Software Developer",
-        altText: "Joshua Kasasira - Developer"
-    },
-    {
         id: 16,
-        courseImg: "https://res.cloudinary.com/filipe256/image/upload/v1642172210/qraftstore/Shakira_Ndagire_Developer_pmgrge.jpg",
-        name: "Mwaka Ceaser.",
-        role: "Software Developer",
-        altText: "Shakira Ndagire - Developer"
-    },
-    {
-        id: 17,
         courseImg: "https://res.cloudinary.com/filipe256/image/upload/v1642172211/qraftstore/Stuart_Stuart_Developer_vmciej.jpg",
         name: "Stuart Kalema",
         role: "Software Developer",
         altText: "Stuart - Developer"
     },
     {
+        id: 17,
+        courseImg: "https://res.cloudinary.com/filipe256/image/upload/v1642172206/qraftstore/Joshua_Kasasira_Developer_ehgije.jpg",
+        name: "Joshua Kasasira",
+        role: "Software Developer",
+        altText: "Joshua Kasasira - Developer"
+    },
+    {
         id: 18,
-        courseImg: "https://res.cloudinary.com/filipe256/image/upload/v1644157368/qraftstore/igxjeiazjt5oiv8ejwwn.png",
-        name: "Ceaser Mwaka",
-        role: "Talent Developer - Visual Art",
-        altText: "Ceaser - Visual Art"
-    }
+        courseImg: "https://res.cloudinary.com/filipe256/image/upload/v1642172204/qraftstore/Anold_Kalema_Developer_bwknkb.jpg",
+        name: "Anold Kalema",
+        role: "Software Developer",
+        altText: "Anold - Developer"
+    },
 ]
 
 export default function OurTeam() {
     return (
         // start of team area
         <div className="mb-6 pb-4 px-6 sm:px-9">
-                <h2 className="font-bold text-blue-900  text-4xl mb-4 mt-20">Our Team</h2>
-                <p className="font-normal text-gray-700 text-xl mb-14">Having more experienced members and mentors that have devoted to the Vision</p>
-                <div className=" mt-6 grid gap-y-6 md:grid-cols-4 md:gap-x-10  sm:grid-cols-2 sm:gap-10 ">
+            <h2 className="font-bold text-blue-900  text-4xl mb-4 mt-20">Our Team</h2>
+            <p className="font-normal text-gray-700 text-xl mb-14">Having more experienced members and mentors that have devoted to the Vision</p>
+            <div className=" mt-6 grid gap-y-6 md:grid-cols-4 md:gap-x-10  sm:grid-cols-2 sm:gap-10 ">
                 {
                     data.map(mentor => (
                         <div key={mentor.id} className="flex justify-center">
                             <div>
                                 <div className="flex justify-center">
-                                    <Image 
-                                            src={mentor.courseImg} alt={mentor.altText} 
-                                            width={250}
-                                            height={250}
-                                            quality={100}
-                                            className="flex justify-center"
-                                        />
+                                    <Image
+                                        src={mentor.courseImg} alt={mentor.altText}
+                                        width={230}
+                                        height={220}
+                                        quality={100}
+                                        className="flex justify-center"
+                                    />
                                 </div>
                                 <div className="flex flex-col justify-center p-2">
                                     <h3 className="text-xl text-gray-700 font-medium flex justify-center mb-0">{mentor.name}</h3>
                                     <h3 className="text-medium font-semibold flex text-blue-900 justify-center mt-2">{mentor.role}</h3>
                                     <ul className=" flex justify-center w-full mt-2">
-                                        <li className="text-blue-600 "> 
-                                            <Link href="#"> 
+                                        <li className="text-blue-600 ">
+                                            <Link href="#">
                                                 <a className="text-2xl">
                                                     <AiFillLinkedin className="mr-1" />
                                                 </a>
-                                            </Link> 
+                                            </Link>
                                         </li>
-                                        <li className="text-light-blue"> 
-                                            <Link href="#"> 
+                                        <li className="text-light-blue">
+                                            <Link href="#">
                                                 <a className="text-2xl">
                                                     <AiFillTwitterSquare className="mr-1" />
                                                 </a>
-                                            </Link> 
+                                            </Link>
                                         </li>
-                                        <li className="text-blue-900 "> 
-                                            <Link href="#"> 
-                                                <a className="text-2xl">
-                                                    <AiFillFacebook className="mr-1"/>
-                                                </a>
-                                            </Link> 
-                                        </li>
-                                        <li className="text-red-400 "> 
+                                        <li className="text-blue-900 ">
                                             <Link href="#">
-                                            <a className="text-2xl">
-                                                <AiFillInstagram className="mr-1"/>
-                                            </a>    
-                                            </Link> 
+                                                <a className="text-2xl">
+                                                    <AiFillFacebook className="mr-1" />
+                                                </a>
+                                            </Link>
+                                        </li>
+                                        <li className="text-red-400 ">
+                                            <Link href="#">
+                                                <a className="text-2xl">
+                                                    <AiFillInstagram className="mr-1" />
+                                                </a>
+                                            </Link>
                                         </li>
                                     </ul>
                                 </div>
                             </div>
-                    </div>
+                        </div>
                     ))
                 }
             </div>
