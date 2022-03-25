11b028e8c50acca82eb5f7c00cab130af71f7995
mambop <pbiscolt@gmail.com>
2022-03-25 10:06:40 +0300

    Docs: changes in documentation

---
+++ pages/designers/dintonReg.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const dintonReg = () => {
+  return (
+    <div>dintonReg</div>
+  )
+}
---
+++ pages/developer/martheReg.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const martheReg = () => {
+  return (
+    <div>martheReg</div>
+  )
+}
