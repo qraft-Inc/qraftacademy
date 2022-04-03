70ec4648dc24db0eb3cdfc7d57f63ca68105d4e2
dinton harold <dintonharold@gmail.com>
2022-04-03 17:14:32 +0300

    Style: style changes and making the form responsive

--- pages/designers/dintonReg.js
+++ pages/designers/dintonReg.js
@@ -3,7 +3,7 @@ import { FaAsterisk } from "react-icons/fa";
 export default function dintonReg() {
   return (
     // INTRODUCTION
-    <div className="container mt-40 mx-auto mb-40 w-1/2">
+    <div className="container mt-40 mx-auto mb-40 w-4/5 sm:w-3/5">
       <div
         className="container flex flex-col space-y-6 items-start justify-center px-6 py-10 border rounded border-black"
         // style={{ width: 900, height: 498.34 }}
@@ -12,7 +12,7 @@ export default function dintonReg() {
           className="text-3xl sm:text-4xl font-extrabold w-full"
           // style={{ width: 852, height: 194.34 }}
         >
-          QRAFT NIGHT SCHOOL APPLICATION FORM-UI/UX Track 2022
+          QRAFT NIGHT SCHOOL APPLICATION FORM-UI/UX TRACK 2022
         </p>
         <p className="text-xl sm:text-2xl font-semibold w-full">
           Life is an ongoing learning process and this Night school gives you an
@@ -36,9 +36,9 @@ export default function dintonReg() {
             <FaAsterisk size="0.5em" color="red" />
           </div>
           {/* email input with placeholder */}
-          <div className=" flex flex-col  items-start justify-start  border-b border-gray-400">
+          <div className=" flex flex-col  items-start justify-start  border-b border-gray-400 w-5/6">
             <input
-              className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight outline-none focus:border-gray-400 placeholder:italic"
+              className="appearance-none bg-transparent  w-full text-gray-700  border-none leading-tight mt-0 px-0.5 border-b border-gray-400 focus:ring-0 focus:border-gray-700"
               type="email"
               placeholder="Your email"
               aria-label="email"
@@ -57,9 +57,9 @@ export default function dintonReg() {
             <FaAsterisk size="0.5em" color="red" />
           </div>
           {/* Full name input with placeholder */}
-          <div className=" flex flex-col  items-start justify-start  border-b border-gray-400">
+          <div className=" flex flex-col  items-start justify-start border-b border-gray-400 w-5/6">
             <input
-              className="appearance-none bg-transparent  w-full text-gray-700  border-none leading-tight  "
+              className="appearance-none bg-transparent  w-full text-gray-700  border-none leading-tight mt-0 px-0.5 border-b border-gray-400 focus:ring-0 focus:border-gray-700"
               type="text"
               placeholder="Your answer"
               aria-label="fullname"
@@ -78,9 +78,9 @@ export default function dintonReg() {
             <FaAsterisk size="0.5em" color="red" />
           </div>
           {/* telephone input with placeholder */}
-          <div className=" flex flex-col  items-start justify-start  border-b border-gray-400">
+          <div className=" flex flex-col items-start justify-start  border-b border-gray-400 w-5/6">
             <input
-              className="appearance-none bg-transparent  w-full text-gray-700  border-none leading-tight  "
+              className="appearance-none bg-transparent  w-full text-gray-700  border-none leading-tight mt-0 px-0.5 border-b border-gray-200 focus:ring-0 focus:border-black "
               type="tel"
               placeholder="Your answer"
               aria-label="telephone"
@@ -99,9 +99,9 @@ export default function dintonReg() {
             <FaAsterisk size="0.5em" color="red" />
           </div>
           {/* cv input with placeholder */}
-          <div className=" flex flex-col  items-start justify-start  border-b border-gray-400">
+          <div className=" flex flex-col  items-start justify-start  border-b border-gray-400 w-5/6">
             <input
-              className="appearance-none bg-transparent  w-full text-gray-700  border-none leading-tight  "
+              className="appearance-none bg-transparent  w-full text-gray-700  border-none leading-tight focus:ring-0 "
               type="url"
               placeholder="Your answer"
               aria-label="cv"
@@ -120,9 +120,9 @@ export default function dintonReg() {
             <FaAsterisk size="0.5em" color="red" />
           </div>
           {/* cover letter input with placeholder */}
-          <div className=" flex flex-col  items-start justify-start  border-b border-gray-400">
+          <div className=" flex flex-col  items-start justify-start  border-b border-gray-400 w-5/6">
             <input
-              className="appearance-none bg-transparent  w-full text-gray-700  border-none leading-tight  "
+              className="appearance-none bg-transparent  w-full text-gray-700  border-none leading-tight focus:ring-0 "
               type="url"
               placeholder="Your answer"
               aria-label="letter"
@@ -150,7 +150,7 @@ export default function dintonReg() {
               id="product_design"
               value="Product Design"
             />
-            <label for="product_design" className="ml-4">
+            <label for="product_design" className="ml-4 text-sm sm:text-base">
               Product Design
             </label>
             <br></br>
@@ -160,7 +160,10 @@ export default function dintonReg() {
               id="digital_marketing"
               value="Digital Marketing"
             />
-            <label for="digital_marketing" className="ml-4">
+            <label
+              for="digital_marketing"
+              className="ml-4 text-sm sm:text-base"
+            >
               Digital Marketing
             </label>
             <br></br>
@@ -171,7 +174,10 @@ export default function dintonReg() {
               value="Software Development"
               className="form-radio"
             />
-            <label for="software_development" className="ml-4">
+            <label
+              for="software_development"
+              className="ml-4 text-sm sm:text-base"
+            >
               Software Development
             </label>
           </div>
@@ -188,10 +194,9 @@ export default function dintonReg() {
             </label>
             <FaAsterisk size="0.5em" color="red" />
           </div>
-          <div className="flex flex-col space-y-6 items-start justify-start py-2">
+          <div className="flex flex-col space-y-6 items-start justify-start py-2 w-5/6">
             <textarea
               className="
-        form-control
         block
         w-full
         px-4
@@ -224,7 +229,7 @@ export default function dintonReg() {
             </label>
             <FaAsterisk size="0.5em" color="red" />
           </div>
-          <div className="flex flex-col space-y-6 items-start justify-start py-2">
+          <div className="flex flex-col space-y-6 items-start justify-start py-2 w-5/6">
             <textarea
               className="
         form-control
@@ -261,7 +266,7 @@ export default function dintonReg() {
             </label>
             <FaAsterisk size="0.5em" color="red" />
           </div>
-          <div className="flex flex-col space-y-6 items-start justify-start py-2">
+          <div className="flex flex-col space-y-6 items-start justify-start py-2 w-5/6">
             <textarea
               className="
         form-control
@@ -297,7 +302,7 @@ export default function dintonReg() {
             </label>
             <FaAsterisk size="0.5em" color="red" />
           </div>
-          <div className="flex flex-col space-y-6 items-start justify-start py-2">
+          <div className="flex flex-col space-y-6 items-start justify-start py-2 w-5/6">
             <textarea
               className="
         form-control
@@ -333,7 +338,7 @@ export default function dintonReg() {
             </label>
             <FaAsterisk size="0.5em" color="red" />
           </div>
-          <div className="flex flex-col space-y-6 items-start justify-start py-2">
+          <div className="flex flex-col space-y-6 items-start justify-start py-2 w-5/6">
             <textarea
               className="
         form-control
@@ -376,7 +381,10 @@ export default function dintonReg() {
               id="highly_independent_role"
               value="Highly independent role"
             />
-            <label for="highly_independent_role" className="ml-4">
+            <label
+              for="highly_independent_role"
+              className="ml-4 text-sm sm:text-base"
+            >
               Highly independent role
             </label>
             <br></br>
@@ -387,7 +395,7 @@ export default function dintonReg() {
               id="team_based"
               value="Team Based"
             />
-            <label for="team_based" className="ml-4">
+            <label for="team_based" className="ml-4 text-sm sm:text-base">
               Team Based
             </label>
             <br></br>
@@ -397,7 +405,10 @@ export default function dintonReg() {
               id="strong_people_skills"
               value="Strong people skills Role"
             />
-            <label for="strong_people_skills" className="ml-4">
+            <label
+              for="strong_people_skills"
+              className="ml-4 text-sm sm:text-base"
+            >
               Strong people skills Role
             </label>
             <br></br>
@@ -407,7 +418,10 @@ export default function dintonReg() {
               id="less_people_focused"
               value="Less people focused Role"
             />
-            <label for="less_people_focused" className="ml-4">
+            <label
+              for="less_people_focused"
+              className="ml-4 text-sm sm:text-base"
+            >
               Less people focused Role
             </label>
           </div>
@@ -424,7 +438,7 @@ export default function dintonReg() {
             </label>
             <FaAsterisk size="0.5em" color="red" />
           </div>
-          <div className="flex flex-col space-y-6 items-start justify-start py-2">
+          <div className="flex flex-col space-y-6 items-start justify-start py-2 w-5/6">
             <textarea
               className="
         form-control
@@ -460,7 +474,7 @@ export default function dintonReg() {
             </label>
             <FaAsterisk size="0.5em" color="red" />
           </div>
-          <div className="flex flex-col space-y-6 items-start justify-start py-2">
+          <div className="flex flex-col space-y-6 items-start justify-start py-2 w-5/6">
             <textarea
               className="
         form-control
@@ -477,7 +491,7 @@ export default function dintonReg() {
         transition
         ease-in-out
         m-0
-        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none 
       "
               id="your_goal"
               rows="3"
@@ -485,9 +499,9 @@ export default function dintonReg() {
             ></textarea>
           </div>
         </div>
-        <div className="container mx-auto">
+        <div className="container">
           <button
-            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-auto"
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 sm:py-2 sm:px-4 rounded mx-auto"
             type="submit"
           >
             Submit
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,31 @@
 module.exports = {
-  mode:"jit",
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}',
-  './components/**/*.{js,ts,jsx,tsx}',
-  "./node_modules/tw-elements/dist/js/**/*.js"],
+  mode: "jit",
+  purge: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/tw-elements/dist/js/**/*.js",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
-        "light-blue": "#4092CF"
+        "light-blue": "#4092CF",
       },
       spacing: {
-        '35': '7rem'
+        35: "7rem",
       },
-      maxWidth:{
-        'ls': '20rem'
+      maxWidth: {
+        ls: "20rem",
       },
-      fontWeight:{
-        'semiMedium': '400'
+      fontWeight: {
+        semiMedium: "400",
       },
-      fontWeight:{
-        'semiMedium': '400'
+      fontWeight: {
+        semiMedium: "400",
       },
-      screens:{
-        'xsm': '390px',
-      // => @media (min-width: 390px) { ... }
-      
+      screens: {
+        xsm: "390px",
+        // => @media (min-width: 390px) { ... }
       },
     },
   },
@@ -32,7 +33,9 @@ module.exports = {
     extend: {},
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('tw-elements/dist/plugin'),
+    require("@tailwindcss/forms")({
+      strategy: "base" // only generate global styles
+    }),
+    require("tw-elements/dist/plugin"),
   ],
-}
+};
