cbd05efba2688db8575a01a9e2ca8e8d090792ff
mambop <pbiscolt@gmail.com>
2022-05-27 22:34:58 +0300

    ft:add mongodb

--- pages/api/register/index.js
+++ pages/api/register/index.js
@@ -1,5 +1,12 @@
-import {data} from "./data/db.json"
+import db from "../../../data/db";
 
-export default function handler(req, res) {
-  res.status(200).json(data)
+
+export default async function handler(req, res){
+    await db.connect();
+    await db.disconnect();
+    res.status(200).json({name:"john doe"})
 }
+
+// DB_USER = user
+// DB_PASSWORD = Lw4bEyJlh6KIFnK7
+// DB_URL = mongodb+srv://$DB_USER:$DB_PASSWORD@cluster0.jxwjo.mongodb.net/register?retryWrites=true&w=majority
