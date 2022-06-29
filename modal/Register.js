cfcc043c18c4e4d83744411c6bf9fa327e31c82c
mambop <pbiscolt@gmail.com>
2022-06-29 17:31:34 +0300

    ft:mongodb

--- modal/Register.js
+++ modal/Register.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const registerSchema = new mongoose.Schema(
   {
     email: {type:String, required:true, unique:true},
+    password: { type: String},
     name: {type:String, required:true},
     phone: {type:String, required:true},
     program: {type:String, required:true},
