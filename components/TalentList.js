3fa9ac5a8d96cba2087c5e6a84e0e48fdd37cd1f
tugumeandree <andrewtugume2@gmail.com>
2022-07-27 01:09:30 +0300

    added hire form

--- components/TalentList.js
+++ components/TalentList.js
@@ -8,34 +8,25 @@ const data = [
     {
         id: 1,
         courseImg: "https://res.cloudinary.com/filipe256/image/upload/v1655885737/qraftstore/product_design_nf5mvk.jpg",
-        courseTitle: "Product Designers",
-        text1: "Start",
-        text2: "01.September.2022",
-        text3: "1 Month",
-        altText: "Software Developers",
+        courseTitle: "Hire Product Designers",
+        altText: " Hire Product Designers",
         url: "/hire"
     },
     {
         id: 2,
         courseImg: "https://res.cloudinary.com/filipe256/image/upload/v1655885737/qraftstore/software_developmen_ooqmhl.jpg",
-        courseTitle: "Software Developers",
-        text1: "Start",
-        text2: "05.September.2022",
-        text3: "3 Months",
-        altText: "Digital Marketers",
-        url: "/marketing"
+        courseTitle: "Hire Software Developers",
+        altText: "Hire Software Developers",
+        url: "/hire"
     },
    
    
     {
         id: 5,
         courseImg: "https://cdn.pixabay.com/photo/2018/09/18/11/19/artificial-intelligence-3685928_960_720.png",
-        courseTitle: "Data Scientists",
-        // text1: "Start",
-        text2: "Learn scientific methods, processes, algorithms and systems to extract knowledge and insights from noisy, structured and unstructured data.",
-        // text3: "3 Months",
-        altText: "data scientists",
-        url: "/data-science-course"
+        courseTitle: "Hire Data Scientists",
+        altText: "Hire Data Scientists",
+        url: "/hire"
     }
  
 
@@ -44,7 +35,7 @@ const data = [
 export default function TalentList(props) {
     return (
         <div className="mb-6 py-8 px-0 pt-24" id="talentList">
-            <h2 className="font-medium text-blue-900 text-xl">Available Talent</h2>
+            <h2 className="font-medium text-blue-900 text-xl">Hire Now</h2>
             <div className="mt-4 grid gap-y-6 md:gap-x-10 xl:grid-cols-3 sm:grid-cols-2 sm:gap-10">
                 {data.map(course => (
                     <div key={course.id} className=" flex justify-center xl:justify-even">
