2cc87a73a09d233f0c95817e8f603cb72b7d169c
mambop <pbiscolt@gmail.com>
2022-10-06 19:32:56 +0300

    register edit

--- components/Navbar.js
+++ components/Navbar.js
@@ -72,7 +72,7 @@ export default function Navbar() {
               <Link href="https://dashboard-qraftacademy.netlify.app/">
                 <button
                   type="button"
-                  class="inline-block px-6 py-2 border-2 border-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-full hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
+                  className="inline-block px-6 py-2 border-2 border-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-full hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
                 >
                   Login
                 </button>
@@ -113,7 +113,7 @@ export default function Navbar() {
           <Link href="https://dashboard-qraftacademy.netlify.app/">
             <button
               type="button"
-              class="w-24 py-2 border-2 border-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-full hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
+              className="w-24 py-2 border-2 border-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-full hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
             >
               Login
             </button>
