b07cf09ab1c06fb8b9e248c493e3a5d01564b306
mambop <pbiscolt@gmail.com>
2022-10-18 05:59:58 +0300

    db edit

--- pages/api/user.js
+++ pages/api/user.js
@@ -10,50 +10,58 @@ export default async function handler(req, res) {
   switch (method) {
     // create new user
     case "POST":
+      // try {
+      //   const {
+      //     email,
+      //     fullname,
+      //     telephone,
+      //     cv,
+      //     letter,
+      //     program,
+      //     course,
+      //     stack,
+      //     position,
+      //     linkedin,
+      //     textarea1,
+      //     textarea2,
+      //     textarea3,
+      //     textarea4,
+      //     textarea5,
+      //     role,
+      //     textarea6,
+      //     textarea7,
+      //     image,
+      //     isAdmin,
+      //   } = req.body;
+      //   const newUser = new User({
+      //     email,
+      //     fullname,
+      //     telephone,
+      //     cv,
+      //     letter,
+      //     program,
+      //     course,
+      //     stack,
+      //     position,
+      //     linkedin,
+      //     textarea1,
+      //     textarea2,
+      //     textarea3,
+      //     textarea4,
+      //     textarea5,
+      //     role,
+      //     textarea6,
+      //     textarea7,
+      //     image,
+      //     isAdmin,
+      //   });
+      //   const user = await newUser.save();
+      //   await db.disconnect();
+      //   return res.status(200).json(user);
+      // }
       try {
-        const {
-          email,
-          fullname,
-          telephone,
-          cv,
-          letter,
-          program,
-          course,
-          stack,
-          position,
-          linkedin,
-          textarea1,
-          textarea2,
-          textarea3,
-          textarea4,
-          textarea5,
-          role,
-          textarea6,
-          textarea7,
-          image,
-          isAdmin,
-        } = req.body;
         const newUser = new User({
-          email,
-          fullname,
-          telephone,
-          cv,
-          letter,
-          program,
-          course,
-          stack,
-          position,
-          linkedin,
-          textarea1,
-          textarea2,
-          textarea3,
-          textarea4,
-          textarea5,
-          role,
-          textarea6,
-          textarea7,
-          image,
-          isAdmin,
+          ...req.body,
         });
         const user = await newUser.save();
         await db.disconnect();
