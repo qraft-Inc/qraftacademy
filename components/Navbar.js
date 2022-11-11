531f16dc11b35527611422239de0c5d3c562c0cf
mambop <pbiscolt@gmail.com>
2022-11-11 23:31:02 +0300

    db fix

--- components/Navbar.js
+++ components/Navbar.js
@@ -70,6 +70,7 @@ export default function Navbar() {
                 </a>
               </Link>
               <Link href="https://dashboard-qraftacademy.netlify.app/">
+            
                 <button
                   type="button"
                   className="inline-block px-6 py-2 border-2 border-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-full hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
