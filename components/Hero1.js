680b3534a72120643f228f0326a33fed97bc7ad4
Figurines <a1989@A1989s-MacBook-Pro.local>
2023-02-28 22:53:39 +0300

    added entrepreneurs tab

--- components/Hero1.js
+++ components/Hero1.js
@@ -8,7 +8,7 @@ export default function Hero1() {
         <div className=" h-full absolute inset-0 " style={{ top: "110vh" }}>
           <Image
             alt="Background Image"
-            src="https://res.cloudinary.com/dwa3soopc/image/upload/v1677600066/qraft%20/mikelya-fournier-4iG91rr92Ks-unsplash_lwamc8.jpg"
+            src="https://res.cloudinary.com/dwa3soopc/image/upload/v1677600066/qraft%20/mikelya-fournier-4iG91rr92Ks-unsplash_lwamc8.jpg "
             layout="fill"
             objectFit="cover"
             quality={100}
@@ -18,7 +18,7 @@ export default function Hero1() {
         <div className="px-4 py-44 text-left text-white md:max-w-none md:text-center z-10 relative">
           <h1 className="text-4xl font-extrabold leading-10 tracking-tight md:text-center sm:leading-none md:text-6xl lg:text-7xl">
             <span className="inline-block md:block">
-              Fellowship For Digital Entrepreneurs
+              Fellowship Access To Resources For Digital Entrepreneurs
             </span>
           </h1>
 
