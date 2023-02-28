dac1f99b15a8c49572572861c08c577966b1c810
Figurines <a1989@A1989s-MacBook-Pro.local>
2023-02-28 23:00:05 +0300

    added entrepreneurs tab

--- components/Hero1.js
+++ components/Hero1.js
@@ -18,7 +18,7 @@ export default function Hero1() {
         <div className="px-4 py-44 text-left text-white md:max-w-none md:text-center z-10 relative">
           <h1 className="text-4xl font-extrabold leading-10 tracking-tight md:text-center sm:leading-none md:text-6xl lg:text-7xl">
             <span className="inline-block md:block">
-              Fellowship Access To Resources For Digital Entrepreneurs
+              Fellowship And Access To Resources For Digital Entrepreneurs
             </span>
           </h1>
 
