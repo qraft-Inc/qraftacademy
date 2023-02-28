0c329e55466b564e52bf482e68e05e438df375e7
Figurines <a1989@A1989s-MacBook-Pro.local>
2023-02-28 12:55:38 +0300

    added entrepreneurs tab

---
+++ pages/entrepreneurs.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Entrepreneurs() {
+  return (
+    <iframe
+      style={{ width: "100vw", height: "100vh", paddingTop: 80 }}
+      src="https://forms.gle/a2a5r6KaNgaXr79J7"
+      title="W3Schools Free Online Web Tutorials"
+    ></iframe>
+  );
+}
