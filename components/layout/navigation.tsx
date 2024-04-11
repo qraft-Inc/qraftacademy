1695b671f4190e7eb0f9962fd30bd5dd68d57de7
Atuhaire Collins Benda <45392987+kolynzb@users.noreply.github.com>
2024-04-11 08:35:59 +0000

    style: add debug class

--- components/layout/navigation.tsx
+++ components/layout/navigation.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
 
 const Navigation = () => {
+  const linkStyle = ""
   return (
-    <div className="grid grid-cols-7 grid-rows-5 gap-4">
-    <div className="col-span-2">1</div>
-    <div className="col-start-3">2</div>
-    <div className="col-start-4">3</div>
-    <div className="col-start-5">4</div>
-    <div className="col-start-6">5</div>
-    <div className="col-start-7">6</div>
-</div>
+    <nav className="grid grid-cols-7 grid-rows-5 gap-4 border-primary-400 ">
+      <div className="col-span-2">Qraft</div>
+      <div className="col-start-3 text-center text-primary-400  uppercase font-heading"><p className="">Research</p></div>
+      <div className="col-start-4 ">Technologists</div>
+      <div className="col-start-5 text-primary-400  uppercase font-heading">entreprenuers</div>
+      <div className="col-start-6 text-primary-400  uppercase font-heading">about</div>
+      <div className="col-start-7 bg-primary-100 uppercase debug">login</div>
+    </nav>
   )
 }
 
 export default Navigation
+
