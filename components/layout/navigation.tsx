d2d11858e48680b4d6ebd0262417f4ebe3da729f
Atuhaire Collins Benda <45392987+kolynzb@users.noreply.github.com>
2024-04-12 14:01:24 +0000

    feat: add hero section

--- components/layout/navigation.tsx
+++ components/layout/navigation.tsx
@@ -5,13 +5,13 @@ const Navigation = () => {
   const linkStyle: HTMLAttributes<HTMLDivElement>["className"] = "font-heading uppercase font-bold hover:scale-9"
   const boxStyle: HTMLAttributes<HTMLDivElement>["className"] = "bg-white text-primary-400  h-[6.2vh]  flex items-center justify-center border-primary-400 border border-3 cursor-pointer transition-all ease-in-out duration-30"
   return (
-    <nav className={cn("grid grid-cols-3 md:grid-cols-7 grid-rows-5 ", "border-primary-400")}>
+    <nav className={cn("grid grid-cols-3 md:grid-cols-7 grid-rows-2 md:grid-rows-1 ", "border-primary-400")}>
       <div className={cn("md:col-span-2", boxStyle, "justify-start")}>1</div>
       <div className={cn("md:col-start-3", boxStyle, linkStyle)}>research</div>
       <div className={cn("md:col-start-4", boxStyle, linkStyle)}>technologists</div>
       <div className={cn("md:col-start-5", boxStyle, linkStyle)}>entreprenuers</div>
       <div className={cn("md:col-start-6", boxStyle, linkStyle)}>about</div>
-      <div className={cn("md:col-start-7", boxStyle, linkStyle, "bg-primary-100 text-white")}>login</div>
+      <div className={cn("md:col-start-7", boxStyle, linkStyle, "bg-primary-200  text-white")}>login</div>
     </nav>
   )
 }
--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -7,8 +7,8 @@ import Balancer from 'react-wrap-balancer'
 const Hero = () => {
   return (
     <section className='debug h-screen relative overflow-hidden'>
+      <Image src="/images/hero.png" className="-z-10 object-contain grayscale-[55%] absolute  w-full" alt="" height={1000} width={1000} />
       <GridLines />
-      <Image src="/images/hero.png" className="-z-10 object-cover grayscale-[20%]" alt="" height={1000} width={100} />
     </section>
   )
 }
@@ -16,21 +16,21 @@ const Hero = () => {
 export default Hero
 
 
-const GridLines = () => (<div className={cn("grid grid-cols-5 grid-rows-5 gap-4 ", "h-screen")}>
-  <div className="col-span-3 row-span-3"><HeroContent /></div>
-  <div className="col-span-2 row-span-3 col-start-4">2</div>
-  <div className="col-span-3 row-span-2 row-start-4">3</div>
-  <div className="col-span-2 row-span-2 col-start-4 row-start-4">4</div>
+const GridLines = () => (<div className={cn("grid grid-cols-5 grid-rows-5", "h-screen")}>
+  <div className="col-span-3 row-span-3 border-r-4 border-b-4 border-primary-400 flex items-end "><HeroContent /></div>
+  <div className="col-span-2 row-span-3 col-start-4" />
+  <div className="col-span-3 row-span-2 row-start-4" />
+  <div className="col-span-2 row-span-2 col-start-4 row-start-4 border-l-4 border-t-4 border-primary-400" />
 </div>)
 
 
 const HeroContent = () => (
-  <section className="max-w-sm space-x-1">
-    <Balancer as="h1" className='font-bold uppercase '>Ignite Your Tech Career with Qraft Academy</Balancer>
+  <section className="max-w-lg pl-5 space-x-1 flex flex-col items-start justify-between  space-y-5 pb-6">
+    <Balancer as="h1" className='font-bold uppercase text-4xl'>Ignite Your Tech Career with Qraft Academy</Balancer>
     <p className="font-light ">Join a Community of Tech Innovators, Gain Expert Training, and Create Your Future</p>
-    <div className="bg-white p-1 flex item-center justify-center">
-      <button className="text-xs px-3 px-2 bg-primary-100">JOIN APPRENTISHIP</button>
-      <button className="text-xs  px-3 px-2 bg-white text-white">DISCOVER TALENT</button>
+    <div className="bg-white border-4 border-white   ">
+      <button className="text-xs px-6 py-4 bg-primary-200 font-extrabold  ">JOIN APPRENTISHIP</button>
+      <button className="text-xs px-6 py-4 bg-white text-primary-400 font-extrabold ">DISCOVER TALENT</button>
     </div>
   </section>
 )
