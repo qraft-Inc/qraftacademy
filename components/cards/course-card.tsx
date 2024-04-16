c34c69f0caf89013314bd6f14bf978d6040b54b3
Atuhaire Collins Benda <45392987+kolynzb@users.noreply.github.com>
2024-04-16 18:12:49 +0000

    feat: update course card

--- components/cards/course-card.tsx
+++ components/cards/course-card.tsx
@@ -6,15 +6,15 @@ import Image from "next/image"
 const CourseCard = ({ className }: { className?: string }) => {
     return (
         <div className="flex w-full mb-8 sm:px-4 md:w-1/2 lg:w-1/3 lg:mb-0">
-            <div className="flex flex-grow flex-col p-6 space-y-6 rounded shadow sm:p-8 dark:bg-gray-50">
-                <Image src="/images/gradient.jpeg" height={500} width={500} loading="lazy" alt="ladies discusing qraft" className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500"></Image>
+            <figure className="flex flex-grow flex-col p-6 space-y-6 rounded shadow sm:p-8 dark:bg-gray-50">
+                <Image src="/images/gradient.jpeg" height={500} width={500} loading="lazy" alt="ladies discusing qraft" className="object-cover object-center w-full rounded-t-md h-60 dark:bg-gray-500"></Image>
                 <div className="flex flex-col justify-between p-6 space-y-8">
                     <div className="space-y-2">
-                        <h2 className="text-3xl font-semibold tracking-wide">Product Design Apprenticeship</h2>
-                        <p className="dark:text-gray-800">01 Sept 2023 - 01 Oct 2023</p>
+                        <h2 className=" font-semibold tracking-wide">Product Design Apprenticeship</h2>
+                        <p className="text-primary-100 text-sm font-light ">01 Sept 2023 - 01 Oct 2023</p>
                     </div>
                 </div>
-            </div>
+            </figure>
         </div>
     )
 }
--- components/sections/courses.tsx
+++ components/sections/courses.tsx
@@ -6,7 +6,7 @@ const Courses = () => {
     return (
         <section className='container px-4 mx-auto'>
             <h1 className="text-4xl  tracking-wider  font-bold lg:text-5x mb-2">Available Courses.</h1>
-            <div className="flex flex-wrap items-stretch -mx-4">
+            <div className="flex flex-wrap -mx-4">
                 <CourseCard />
                 <CourseCard />
                 <CourseCard />
