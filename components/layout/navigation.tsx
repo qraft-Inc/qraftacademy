f8b1b1897e3904b68fdec92f83e11b1d787ed5ed
Atuhaire Collins Benda <45392987+kolynzb@users.noreply.github.com>
2024-05-08 06:52:04 +0000

    feat: add labs link

--- components/layout/navigation.tsx
+++ components/layout/navigation.tsx
@@ -1,5 +1,6 @@
 import React, { HTMLAttributes } from 'react'
 import cn from "classnames"
+import Link from "next/link"
 
 const Navigation = () => {
   const linkStyle: HTMLAttributes<HTMLDivElement>["className"] = "font-heading uppercase font-bold hover:scale-9"
@@ -7,9 +8,9 @@ const Navigation = () => {
   return (
     <nav className={cn("grid grid-cols-3 md:grid-cols-7 grid-rows-2 md:grid-rows-1 ", "border-primary-400/40   border-[0.5px] md:border-1")}>
       <div className={cn("md:col-span-2", boxStyle, "justify-start font-extrabold")}>QRAFT</div>
-      <div className={cn("md:col-start-3", boxStyle, linkStyle)}>research</div>
-      <div className={cn("md:col-start-4", boxStyle, linkStyle)}>technologists</div>
-      <div className={cn("md:col-start-5", boxStyle, linkStyle)}>entreprenuers</div>
+      <Link href="https://labs.qraftacademy.org/" className={cn("md:col-start-3", boxStyle, linkStyle)}>research</Link>
+      <div className={cn("md:col-start-4", boxStyle, linkStyle)}>consult</div>
+      <div className={cn("md:col-start-5", boxStyle, linkStyle)}>courses</div>
       <div className={cn("md:col-start-6", boxStyle, linkStyle)}>about</div>
       <div className={cn("md:col-start-7", boxStyle, linkStyle, "bg-primary-200  text-white")}>login</div>
     </nav>
