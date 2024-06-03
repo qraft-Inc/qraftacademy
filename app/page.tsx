1617a1850cbe8b3fbdbfb11de0df87b7c845e20e
Atuhaire Collins Benda <45392987+kolynzb@users.noreply.github.com>
2024-06-03 10:09:54 +0000

    chore: add metadata

--- app/page.tsx
+++ app/page.tsx
@@ -3,23 +3,68 @@ import Testimonials from "@/components/sections/testimonials"
 import Hero from "@/components/sections/hero"
 import { Metadata } from "next"
 import Image from "next/image"
+import { seoKeywords, siteConfig } from "@/config/site"
+
 
 export const metadata: Metadata = {
-  title: "Next.js Enterprise Boilerplate",
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  keywords: seoKeywords,
   twitter: {
     card: "summary_large_image",
+    title: `${siteConfig.name} - ${siteConfig.description}`,
+    description: siteConfig.description,
+    images: [`${siteConfig.url}opengraph-image`],
+    creator: "@Kolynz_b",
   },
   openGraph: {
-    url: "https://next-enterprise.vercel.app/",
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
     images: [
       {
-        width: 1200,
-        height: 630,
-        url: "https://raw.githubusercontent.com/Blazity/next-enterprise/main/.github/assets/project-logo.png",
+        url: `${siteConfig.url}opengraph-image`,
       },
     ],
   },
-}
+  alternates: {
+    types: {
+      'application/rss+xml': `${siteConfig.url}rss.xml`,
+    },
+  },
+  creator: "Atuhaire Collins Benda",
+  publisher: "Atuhaire Collins Benda",
+  metadataBase: new URL(siteConfig.url),
+  authors: [
+    { name: "Atuhaire Collins Benda", url: "https://collinsbenda.com" },
+    { name: "Andrew Tugume" },
+  ],
+  applicationName: siteConfig.name,
+  icons: [
+    {
+      rel: "apple-touch-icon",
+      url: "/favicon/apple-touch-icon.png",
+    },
+    {
+      rel: "mask-icon",
+      url: "/favicon/safari-pinned-tab.svg",
+    },
+    {
+      rel: "icon",
+      url: "/favicon/favicon-32x32.png",
+    },
+    {
+      rel: "icon",
+      url: "/favicon/favicon-16x16.png",
+    },
+  ],
+  manifest: "/favicon/site.webmanifest",
+};
 
 export default function Web() {
   return (
--- config/site.ts
+++ config/site.ts
@@ -1,13 +1,13 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "MpaKuGig",
-  description: "Freelancer Workflow Automation Software",
+  name: "Qraft Academy",
+  description: "",
   logo: {
     dark: "/images/logos/dark.svg",
     light: "/images/logos/light.svg",
   },
-  url: "https://mpakugig.com/",
+  url: "https://qraftacademy.com/",
   navItems: [
     {
       label: "Home",
@@ -34,9 +34,57 @@ export const siteConfig = {
     sponsor: "https://patreon.com/jrgarciadev",
   },
   contacts: {
-    tel: ["+256-701-092-815"],
-    email: "hello@mpakugig.com/",
+    tel: ["+256-701-xx-815"],
+    email: "hello@qraftacademy.com/",
     tele: "+256701092815",
     address: "Plot 851 Serumaga Road, Bukoto, Kampala, Uganda",
   },
 };
+
+
+
+export const seoKeywords =  [
+  "education technology",
+  "online learning",
+  "e-learning platforms",
+  "digital education",
+  "remote learning",
+  "virtual classrooms",
+  "adaptive learning",
+  "personalized learning",
+  "educational technology trends",
+  "future of work",
+  "workplace automation",
+  "remote work",
+  "digital skills",
+  "automation technology",
+  "gig economy",
+  "telecommuting",
+  "workplace flexibility",
+  "research",
+  "academic research",
+  "scientific research",
+  "research studies",
+  "research papers",
+  "research methodologies",
+  "research findings",
+  "Edtech",
+  "Atuhaire Collins Benda",
+  "Tech blog",
+  "Andrew Tugume", "educational apps",
+  "learning management systems",
+  "online courses",
+  "distance education",
+  "blended learning",
+  "virtual reality in education",
+  "artificial intelligence in education",
+  "STEM education",
+  "professional development",
+  "digital transformation",
+  "workforce skills",
+  "job automation",
+  "remote collaboration",
+  "flexible work arrangements",
+  "work-life balance",
+  "innovation research"
+]
