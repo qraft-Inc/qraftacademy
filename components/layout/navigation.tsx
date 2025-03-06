88ec1a9768d77427007d58c786b0bc652d414e13
Atuhaire Collins Benda <45392987+kolynzb@users.noreply.github.com>
2025-03-06 19:35:00 +0000

    feat: add

--- components/layout/navigation.tsx
+++ components/layout/navigation.tsx
@@ -9,22 +9,18 @@ const Navigation = () => {
     <nav className={cn("grid grid-cols-3 md:grid-cols-7 grid-rows-2 md:grid-rows-1 ", "border-primary-400/40   border-[0.5px] md:border-1")}>
       <div className={cn("md:col-span-2", boxStyle, "justify-start font-extrabold")}>QRAFT</div>
       <Link className={cn("md:col-start-3", boxStyle, linkStyle)} href="https://labs.qraftacademy.com/">r&d
-
         <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-blue-500 group-hover:w-full transition-all duration-300" />
       </Link>
-      <div className={cn("md:col-start-4", boxStyle, linkStyle)}>programs
+      <Link className={cn("md:col-start-4", boxStyle, linkStyle)}  href="/courses/freelance">programs
         <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-blue-500 group-hover:w-full transition-all duration-300" />
-
-      </div>
+      </Link>
       <Link className={cn("md:col-start-5", boxStyle, linkStyle)} href="https://classroom.qraftacademy.com/">courses
         <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-blue-500 group-hover:w-full transition-all duration-300" />
-
       </Link>
       <div className={cn("md:col-start-6", boxStyle, linkStyle)}>about
         <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-blue-500 group-hover:w-full transition-all duration-300" />
-
       </div>
-      <div className={cn("md:col-start-7 bg-primary-200 ", boxStyle, linkStyle, " text-white")}>login</div>
+      <div className={cn("md:col-start-7 bg-primary-200 ", boxStyle, linkStyle, "text-white")}>login</div>
     </nav>
   )
 }
