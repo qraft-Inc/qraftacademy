054010db9432925aec42efb1fb4256ecff9cb8d4
Claire Masiga <ClaireMasiga22@gmail.com>
2025-07-16 16:51:39 +0300

    FIXING TAILWIND

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,6 @@
-// Before:
 module.exports = {
-  plugins: [
-    'tailwindcss',
-    'autoprefixer',
-  ],
-}
-
-module.exports = {
-  plugins: [
-    'tailwindcss',
-    'autoprefixer',
-  ],
+  plugins: {
+    '@tailwindcss/postcss': {},
+    autoprefixer: {},
+  },
 }
